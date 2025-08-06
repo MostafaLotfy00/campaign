0f897bcea432713de4486b16f6818bfd0144740a
Omar <95372288+omargamal768@users.noreply.github.com>
2025-08-06 20:48:33 +0300

    remove test

--- sheet/src/main/java/com/contacts/sheet/service/GenesysService.java
+++ sheet/src/main/java/com/contacts/sheet/service/GenesysService.java
@@ -344,7 +344,6 @@ public class GenesysService {
            logger.error("Failed to obtain Access Token for Conversation Details API.");
             return null;
         }
-int x =5;
         String detailsUrl = String.format("https://api.%s/api/v2/analytics/conversations/%s/details", region, conversationId);
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(accessToken);
